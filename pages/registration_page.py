26b83b5cb370b479bab60f683a55e4ee10606edd
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-04-22 16:56:07 +0600

    user_register() changed according to user_generator() helper

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -7,15 +7,15 @@ class RegistrationPage(BasePage):
     NAME = (By.CSS_SELECTOR, "input[placeholder='Name']")
     EMAIL_ADD = (By.CSS_SELECTOR, "input[data-qa='signup-email']")
     SIGNUP_BUTTON = (By.CSS_SELECTOR, "button[data-qa='signup-button']")
-    ACCOUNT_INFO = (By.CSS_SELECTOR, "body > section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h2:nth-child(1) > b:nth-child(1)")
+    ACCOUNT_INFO = (By.CSS_SELECTOR, "h2.title.text-center ~ b")
 
     def __init__(self, driver):
         super().__init__(driver)
     
     #page action
-    def user_register(self, name, email):
-        self.send_key(self.NAME, name)
-        self.send_key(self.EMAIL_ADD, email)
+    def user_register(self, user_data):
+        self.send_key(self.NAME, user_data["name"])
+        self.send_key(self.EMAIL_ADD, user_data["email"])
         self.click(self.SIGNUP_BUTTON)
 
     def is_registration_successful(self):
