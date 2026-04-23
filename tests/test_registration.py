e7a7ed9c81cc6acf8740445af949a70911dcabb9
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-04-23 22:57:20 +0600

    test_valid_user_register() updated

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,4 +1,5 @@
 from pages.registration_page import RegistrationPage
+#from pages.account_info_page import AccountInfoPage
 from utils.screenshots import save_screenshots
 from utils.logger import setup_logging
 from utils.user_helper import generate_user
@@ -10,11 +11,32 @@ def test_valid_user_register(driver):
     reg_page = RegistrationPage(driver)
 
     user_data = generate_user()
-    reg_page.user_register(user_data)
-
+    user_info = {
+        "title": "Mr.",
+        "password": "raf123",
+        "day": "20",
+        "month": 11,
+        "year": "2001",
+        "newsletter": "yes",
+        "offer": "yes",
+        "fname": "Rafin",
+        "lname": "Hasan",
+        "company": "RH",
+        "address": "Uttara, Dhaka",
+        "optional_address": "Dakkhin Khan",
+        "country": 5,
+        "state": "Dhaka",
+        "city": "dhaka",
+        "zipcode": "1230",
+        "mobile": "01484813242"
+    }
+    create_account = reg_page.user_register(user_data)
+    #time.sleep(6)
+    create_account.account_create(user_info) 
+    #time.sleep(10)
 
     try:
-        assert "Enter Account Information" in reg_page.is_registration_successful()
+        assert "account created!" in reg_page.is_registration_successful().lower()
         log.info("Registration Succuessfully Completed")
 
     except Exception as e:
