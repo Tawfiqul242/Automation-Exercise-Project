31a1ce288c416f7b05b4595560af16791904f79f
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-04-24 23:55:55 +0600

    enter_signup_info() & click_signup() updated

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -5,23 +5,27 @@ from pages.account_info_page import AccountInfoPage
 class RegistrationPage(BasePage):
 
     #locators
+    NEW_USER_SIGNUP = (By.CSS_SELECTOR, "div[class='signup-form'] h2")
     NAME = (By.CSS_SELECTOR, "input[placeholder='Name']")
     EMAIL_ADD = (By.CSS_SELECTOR, "input[data-qa='signup-email']")
     SIGNUP_BUTTON = (By.CSS_SELECTOR, "button[data-qa='signup-button']")
-    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "h2[class='title text-center'] b")
 
     def __init__(self, driver):
         super().__init__(driver)
     
     #page action
-    def user_register(self, user_data):
+    def is_new_user_visible(self):
+        return self.is_visible(self.NEW_USER_SIGNUP)
+    
+    def enter_signup_info(self, user_data):
         self.send_key(self.NAME, user_data["name"])
         self.send_key(self.EMAIL_ADD, user_data["email"])
+        
+    def click_signup(self):
         self.click(self.SIGNUP_BUTTON)
-        return AccountInfoPage(self.driver)
+        #return AccountInfoPage(self.driver)
 
-    def is_registration_successful(self):
-        return self.get_text(self.SUCCESS_MESSAGE)
+ 
 
     
 
