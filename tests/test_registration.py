8bc0fa287c965d9e8ed8940dcd57619c9348cf5e
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-04-24 23:57:36 +0600

    updated the test_valid_user_register()

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,5 +1,6 @@
+from pages.home_page import HomePage
 from pages.registration_page import RegistrationPage
-#from pages.account_info_page import AccountInfoPage
+from pages.account_info_page import AccountInfoPage
 from utils.screenshots import save_screenshots
 from utils.logger import setup_logging
 from utils.user_helper import generate_user
@@ -8,10 +9,8 @@ import time
 log = setup_logging(__name__)
 
 def test_valid_user_register(driver):
-    reg_page = RegistrationPage(driver)
-
     user_data = generate_user()
-    user_info = {
+    """  user_info = {
         "title": "Mr.",
         "password": "raf123",
         "day": "20",
@@ -29,17 +28,28 @@ def test_valid_user_register(driver):
         "city": "dhaka",
         "zipcode": "1230",
         "mobile": "01484813242"
-    }
-    create_account = reg_page.user_register(user_data)
-    #time.sleep(6)
-    create_account.account_create(user_info) 
-    #time.sleep(10)
+    } """
+    
+    home = HomePage(driver)
+    signup = RegistrationPage(driver)
+    account = AccountInfoPage(driver)
 
-    try:
-        assert "account created!" in reg_page.is_registration_successful().lower()
-        log.info("Registration Succuessfully Completed")
 
-    except Exception as e:
-        save_screenshots(driver, "Not successful register")
-        log.error(f"Failed to signup: {e}")
-        raise
+    assert home.is_homepage_visible()
+    home.click_signup()
+    assert signup.is_new_user_visible()
+    signup.enter_signup_info(user_data)
+    time.sleep(5)
+    signup.click_signup()
+    assert account.is_enter_account_info_visible()
+    account.enter_create_account_info(user_data)
+    time.sleep(10)
+    account.click_create_account()
+    assert account.is_account_created_visible()
+    account.click_create_continue()
+    assert account.is_logged_in_visible()
+    account.click_delete()
+    assert account.is_account_deleted_visible()
+    account.click_delete_continue()
+
+
