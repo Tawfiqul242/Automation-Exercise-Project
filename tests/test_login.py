a480b965be71b3bb54ba63f88328405e1e5fd20f
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-04-26 23:43:37 +0600

    updating test_login_user_with_valid_data() test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,27 +1,59 @@
 import logging
 from pages.login_page import LoginPage
+from pages.home_page import HomePage
+from pages.account_info_page import AccountInfoPage
 from utils.screenshots import save_screenshots 
-from utils.logger import setup_logging
  
-log = setup_logging(__name__)
+log = logging.getLogger(__name__)
 
-def test_login_invalid_user(driver):
-    log.info("testing invalid user login")
-    login_page = LoginPage(driver)
-
+def test_login_user_with_valid_data(driver, registered_user): # test login with valid credentials
+    # page class objects
+    login = LoginPage(driver)
+    home = HomePage(driver)
+    account = AccountInfoPage(driver)
     
-    email = "emailgmail.com"
-    password = 74125
-    login_page.login(email, password)
-
+    log.info("--Starting login with valid credentials test")
     try:
-        assert "Your email or password is incorrect!" in login_page.is_login_invalid()
-        log.info("Checking the error message for invalid login")
-        
-    except Exception as e:
-        save_screenshots(driver,"test_login_invalid_user")
-        log.error(f"Failed to show the error message: {e}")
+        #verify home page is visible
+        assert home.is_homepage_visible(), "Home page is not visible"
+        log.info("Home page is visible")
+
+        home.click_signup_login_btn()
+        log.info("cliciing signup/login button from navbar")
+
+        #verify "Log in to your account" is visible
+        assert login.is_login_to_your_account_visible(), "Log in to your account is not visible"
+        log.info("Login to your account is visible")
+
+        login.fill_login_form(registered_user)
+        login.click_login()
+        log.info("Clicking login button")
+
+        #verify "Logged in user name is visible"
+        assert login.is_logged_in_as_user_visible(), "Logged in as user is not visible"
+        log.info("Logged in as user is visible")
+
+        account.click_delete()
+        log.info("Clicking Delete button")
+
+        #verify "Account Deleted is visible"
+        assert account.is_account_deleted_visible(), "Account Deleted is not visible"
+        log.info("Account Deleted is visible")
+
+        log.info("--Login with valid credentials test is completed")
+
+    except AssertionError as e:
+        log.error(f"Assertion Failed:{e}") 
+        save_screenshots(driver, "Login with valid user credentails test failed")
         raise
+
+    except Exception as e:
+        log.error(f"Unexpected error in test: {e}")
+        save_screenshots(driver,"login with valid data test failed")
+        raise
+
+    finally:
+        log.info("--Login With Valid Data Test Execution Finished--")
         
     
 
