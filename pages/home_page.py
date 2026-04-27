7dd6d6a3366bb06a537ecb3d8207411dad3f6949
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-04-27 14:23:15 +0600

    updated page action methods

--- pages/home_page.py
+++ pages/home_page.py
@@ -3,9 +3,8 @@ from pages.base_page import BasePage
 
 class HomePage(BasePage):
     #locators
+    HOME_BTN = (By.XPATH, "//a[normalize-space()='Home']")
     SIGNUP_LOGIN_BTN = (By.CSS_SELECTOR, "a[href='/login']")
-    
-    
     LOGOUT_BTN = (By.CSS_SELECTOR, "a[href='/logout']")
 
     def __init__(self, driver):
@@ -17,3 +16,9 @@ class HomePage(BasePage):
     
     def click_signup_login_btn(self):
         self.click(self.SIGNUP_LOGIN_BTN)
+
+    def click_logout_btn(self):
+        self.click(self.LOGOUT_BTN)
+
+    def click_home_btn(self):
+        self.click(self.HOME_BTN)
