0e5acabcff87ccb66fcd906c433aeaf21e5e8d2a
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-05-01 23:45:17 +0600

    updated locators & contact_us action method

--- pages/home_page.py
+++ pages/home_page.py
@@ -6,6 +6,11 @@ class HomePage(BasePage):
     HOME_BTN = (By.XPATH, "//a[normalize-space()='Home']")
     SIGNUP_LOGIN_BTN = (By.CSS_SELECTOR, "a[href='/login']")
     LOGOUT_BTN = (By.CSS_SELECTOR, "a[href='/logout']")
+    PRODUCTS_BTN = (By.CSS_SELECTOR, "a[href='/products']")
+    CART_BTN = (By.XPATH, "//body[1]/header[1]/div[1]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[3]/a[1]")
+    TEST_CASES_BTN = (By.XPATH, "//a[contains(text(),'Test Cases')]")
+    API_TESTING_BTN = (By.XPATH, "//a[normalize-space()='API Testing']")
+    cONTACT_US_BTN = (By.XPATH, "//a[normalize-space()='Contact us']")
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -22,3 +27,6 @@ class HomePage(BasePage):
 
     def click_home_btn(self):
         self.click(self.HOME_BTN)
+
+    def click_contact_us_btn(self):
+        self.click(self.cONTACT_US_BTN)
