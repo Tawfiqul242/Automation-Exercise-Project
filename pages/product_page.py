fedb4c1db5b83aa288da7100e0ca17399c154347
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-05-03 22:19:21 +0600

    updated locators & page action methods

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,9 +7,10 @@ class ProductPage(BasePage):
     SEARCH_BAR_BTN = (By.CSS_SELECTOR, ".fa.fa-search")
     CATEGORY_TITLE = (By.XPATH, "//h2[normalize-space()='Category']")
     ALL_PRODUCT_TITLE = (By.CSS_SELECTOR, ".title.text-center")
-    PRODUCT_LIST = (By.CLASS_NAME, "features_items")
+    PRODUCT_LIST = (By.CSS_SELECTOR, ".features_items .product-image-wrapper")
     ADD_TO_CART_BTN = (By.XPATH, "(//a[@class='btn btn-default add-to-cart'][normalize-space()='Add to cart'])[2]")
     VIEW_PRODUCT_BTN = (By.CSS_SELECTOR, "a[href='/product_details/1']")
+    SEARCHED_PRODUCT_TITLE = (By.XPATH, "//h2[normalize-space()='Searched Products']")
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -26,4 +27,25 @@ class ProductPage(BasePage):
 
     def click_view_product_btn(self):
         self.click(self.VIEW_PRODUCT_BTN)
+
+    def fill_search_bar(self, value):
+        self.send_key(self.SEARCH_BAR, value)
+
+    def click_search_btn(self):
+        self.click(self.SEARCH_BAR_BTN)
+
+    def is_searched_product_title_visible(self):
+        return self.is_visible(self.SEARCHED_PRODUCT_TITLE)
+    
+    def get_all_product(self):
+        products = self.wait_for_elements_visibility(self.PRODUCT_LIST)
+  
+        names = []
+        for product in products:
+            name = product.find_element(By.CSS_SELECTOR, ".productinfo p").text
+            names.append(name)
+    
+        return names
+
+
     
