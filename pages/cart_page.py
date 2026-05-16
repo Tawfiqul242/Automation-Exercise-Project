66e99993c4881a8a7ed9f902bdac9d1f3f86fc60
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-05-16 21:14:42 +0600

    updated with get_remove_btn() & is_cart_is_empty_title_visible()

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -13,8 +13,10 @@ class CartPage(BasePage):
     CART_PRODUCTS_QUANTITY = (By.XPATH, "//td[contains(@class,'cart_quantity')]//button")
     CART_PRODUCTS_TOTAL = (By.XPATH, "//td[contains(@class,'cart_total')]//p")
     SHOPPING_CART_TITLE = (By.XPATH, "//li[@class='active']")
+    ITEM_REMOVE_BTN = (By.XPATH, "//td[contains(@class,'cart_delete')]//a")
     PROCEED_TO_CHECKOUT = (By.XPATH, "//a[normalize-space()='Proceed To Checkout']")
     CHECKOUT_LOGIN_SIGNUP_BTN = (By.XPATH, "//u[normalize-space()='Register / Login']")
+    CART_IS_EMPTY_TITLE = (By.XPATH, "//b[normalize-space()='Cart is empty!']")
 
 
     def __init__(self, driver):
@@ -43,6 +45,12 @@ class CartPage(BasePage):
 
     def get_totals(self):
         return [e.text for e in self.wait_for_elements_visibility(self.CART_PRODUCTS_TOTAL)]
+    
+    def get_remove_btn(self):
+        return [e for e in self.wait_for_elements_visibility(self.ITEM_REMOVE_BTN)]
+    
+    def is_cart_is_empty_title_visible(self):
+        return self.is_visible(self.CART_IS_EMPTY_TITLE)
     
     def click_proceed_to_checkout_btn(self):
         self.click(self.PROCEED_TO_CHECKOUT)
